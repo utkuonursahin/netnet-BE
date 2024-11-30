9be66f4b5e067cc0dd9e59f2f161cc5b11d99cc4
utkuonursahin <sahinutkuonur@gmail.com>
2024-11-30 21:14:21 +0300

    fix: put creationtimestamp and updatetimestamp

--- src/main/java/me/utku/netnetbe/model/BaseEntity.java
+++ src/main/java/me/utku/netnetbe/model/BaseEntity.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -17,6 +19,10 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @CreationTimestamp
     private Instant createdAt;
+    
+    @UpdateTimestamp
     private Instant updatedAt;
 }
